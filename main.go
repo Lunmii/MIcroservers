4a922e9303f1e228b8a5ede652b738290869e8d4
Alo Oluwapelumi David <pelumidavid123456@gmail.com>
2023-01-15 22:43:39 +0100

    Creating an Handler Goodbye Handler Go server Configuration Shutdown Parameters Refactoring

--- main.go
+++ main.go
@@ -1,27 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"Learning_Microservices/handlers"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
+
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(w, "Nooo!!", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
+	}()
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-		fmt.Fprintf(w, "Hello %s", d)
-	})
+	sig := <-sigChan
+	l.Println("received terminate, graceful shutdown", sig)
 
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye World")
-	})
-
-	http.ListenAndServe(":9090", nil)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
