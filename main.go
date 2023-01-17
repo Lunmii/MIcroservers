0cfcd0d0fa1ac6c4f22cedff9c7e09643b75dcac
Alo Oluwapelumi David <pelumidavid123456@gmail.com>
2023-01-17 23:10:14 +0100

    Refactored Using gorilla mux Extracted IDS and Vars Validated Request

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"Learning_Microservices/handlers"
 	"context"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
@@ -15,13 +16,21 @@ func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
 	ph := handlers.NewProduct(l)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
+
+	//sm.Handle("/products", ph)
 
 	s := &http.Server{
 		Addr:         ":9090",
